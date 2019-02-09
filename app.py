c4c8edad7fa7595ea05ab37bd5e42487566b5bd1
rifkegribenes <rifkegribenes@gmail.com>
2019-02-08 21:52:30 -0800

    add import db when running app on heroku

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.secret_key = 'fawe8jyhnklj;mja8wi2;'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-  db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
